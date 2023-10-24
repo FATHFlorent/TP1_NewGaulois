65648d0db4abb4f227d93f2a774131e7d9ee63e2
FATHFlorent <116275647+FATHFlorent@users.noreply.github.com>
2023-10-24 19:46:47 +0200

    TP4

--- src/personnages/commercant.java
+++ src/personnages/commercant.java
@@ -1,5 +1,23 @@
 package personnages;
 
-public class commercant {
+public class commercant extends Humain {
 
+	public commercant(String nom, int argent) {
+		super(nom, "Thé", argent);
+	}
+
+	public void seFaireExtorquer() {
+		this.perdreArgent(getArgent());
+		String texte = "J'ai tout perdu! Le monde est trop injuste";
+		parler(texte);
+	}
+	
+	public void recevoir(int argent) {
+		String texte = "";
+		texte += argent;
+		texte += " sous! Je te remerci généreux donnateur!";
+		parler(texte);
+		this.gagnerArgent(argent);
+	}
+		
 }
